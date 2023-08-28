87dafb1cad747af33f0e919e85670fc47300f207
OPSPESH <oliverflynnyparr@gmail.com>
2023-08-28 14:04:26 +0100

    Initial commit

---
+++ src/main/java/net/opspesh/betterfarming/Betterfarmingclient.java
@@ -0,0 +1,10 @@
+package net.opspesh.betterfarming;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Betterfarmingclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
